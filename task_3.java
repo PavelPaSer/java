8cf7f082a707de617ac13737b86f1db602758c6f
«Pashkov <pashkov921326@gmail.com>
2022-11-24 19:54:06 +0300

    Задания к первому семинару

--- task_3.java
+++ task_3.java
@@ -8,17 +8,17 @@ public class  task_3 {
 
     public static void main(String[] args) {
 
-            try (Scanner in_1 = new Scanner(System.in)) {
+            try (Scanner input_1 = new Scanner(System.in)) {
                 System.out.print("Введите первое число: ");
-                double numbers_1 = in_1.nextDouble();
+                double numbers_1 = input_1.nextDouble();
 
-                try (Scanner in_2 = new Scanner(System.in)) {
+                try (Scanner input_2 = new Scanner(System.in)) {
                     System.out.print("Введите второе число: ");
-                    double numbers_2 = in_2.nextDouble();
+                    double numbers_2 = input_2.nextDouble();
                 
-                try (Scanner in_3 = new Scanner(System.in)) {
+                try (Scanner input_3 = new Scanner(System.in)) {
                     System.out.print("Выберите тип операции\n1 - сложить\n2 - вычесть\n3 - умножить\n4 - разделить\n");
-                    int numbers_3 = in_3.nextInt();
+                    int numbers_3 = input_3.nextInt();
                     double result = 0;
 
                     if (numbers_3 == 1) {
